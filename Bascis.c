308b9b69a7da1ce9760c653876cb523c8c0ccfea
OnirudhaGS <ssumo4912@gmail.com>
2025-05-13 21:27:30 +0600

    Biodata
    Homework 13.03.25

---
+++ Bascis.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+printf("Onirudha Ghose Sumo\n"); //Name
+printf("251-15-132\n"); //ID
+printf("Computer Science and Engineering\n"); //Department
+printf("Daffodil International University\n"); //University
+printf("Notre Dame College, Mymensingh\n"); //College
+printf("PRAN-RFL Public School\n"); //School
+printf("Narsingdi\n"); //Home town
+printf("Searching for place to live\n"); //Present address
+/*Multiple Line comment testing
+typing typing  1, 2, 3, 4 blah blah blah......*/
+return 0;
+}
